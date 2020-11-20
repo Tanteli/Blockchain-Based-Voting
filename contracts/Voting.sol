eea4eb79f57106c6c9bbb60af8aa07bee9778c5e
tanteli <andriatanteli@gmail.com>
2020-11-20 18:09:53 +0100

    testing startProposalSession

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,6 +5,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
 
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    WorkflowStatus public status;
+
     mapping(address => bool) public whiteList;
 
     function registerVoter (address _voterAddress) public onlyOwner returns (bool) {
@@ -14,6 +25,10 @@ contract Voting is Ownable {
 
     function isVoter (address _address) public view returns (bool) {
         return whiteList[_address];
+    }
+
+    function startProposalSession() public onlyOwner {
+        status = WorkflowStatus.ProposalsRegistrationStarted;
     }
   
 }
--- test/Voting.test.js
+++ test/Voting.test.js
@@ -1,5 +1,7 @@
 const Voting = artifacts.require("Voting");
 const { expect } = require('chai');
+const { assert } = require('console');
+const { BN } = require('@openzeppelin/test-helpers');
 
 contract("Voting", (accounts) => {
     const admin = accounts[0];
@@ -7,8 +9,14 @@ contract("Voting", (accounts) => {
     const voter2 = accounts[2];
     const voter3 = accounts[3];
 
+    let VotingInstance;
+    
+    beforeEach(async function () {
+        VotingInstance = await Voting.new({from: admin});
+      });
+
     it("Adminstrator should add a Voter to whitelist", async () => {
-        const VotingInstance = await Voting.new({from: admin});
+        
 
         const isVoterBefore = await VotingInstance.isVoter(voter1, {from: admin});
         expect(isVoterBefore).to.equal(false);
@@ -18,4 +26,23 @@ contract("Voting", (accounts) => {
         const isVoterAfter = await VotingInstance.isVoter(voter1, {from: admin});
         expect(isVoterAfter).to.equal(true);
     });
+
+    it("Should start proposal registration session", async () => {
+        // Given
+        let statusBeforeStart = await VotingInstance.status();
+
+        // When
+        await VotingInstance.startProposalSession({from: admin});
+        let statusAfterStart = await VotingInstance.status();
+
+        // Then
+        expect(statusBeforeStart).to.be.bignumber.equal(new BN(0));
+        expect(statusBeforeStart).to.be.bignumber.equal(new BN(1));
+
+
+        
+        
+
+        
+    })
 });
